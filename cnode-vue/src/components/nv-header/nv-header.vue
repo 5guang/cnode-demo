b64220b30d0433f65849c0a33c1873c9824edbd1
static <15701379752@163.com>
2018-05-15 23:26:57 +0800

    首页菜单栏

--- cnode-vue/src/components/nv-header/nv-header.vue
+++ cnode-vue/src/components/nv-header/nv-header.vue
@@ -1,16 +1,55 @@
 <template>
-  <section>
-      <p>hello world</p>
-  </section>
+  <header class="cnode_header">
+      <div class="cnode_header--go_home">
+        <h1 class="cnode_header--go_home-text">CNODE</h1>
+      </div>
+  </header>
 </template>
 
 <script>
 export default {
   name: 'nv-header',
   data() {
-      return {}
+    return {
+      menu: [
+        { label: '01', value: '全部' },
+        { label: '02', value: '精华' },
+        { label: '03', value: '分享' },
+        { label: '04', value: '问答' },
+        { label: '05', value: '招聘' },
+      ]
+    };
   },
   props: {}
-}
+};
 </script>
+
+<style lang="less" scoped>
+@height: 1rem;
+@border-color: #dbdbdb;
+.cnode_header {
+  display: block;
+  width: 100%;
+  height: @height;
+  border-bottom: 1px solid @border-color;
+  background-color: rgb(68, 68, 68);
+  background-size: contain;
+  position: relative;
+  top: 0;
+  left: 0;
+  box-shadow: 0 1px #e8e8e8;
+  &--go_home{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-text{
+      color: #5f99b1;
+      font-family:Comic Sans MS
+    }
+  }
+}
+</style>
+
+
 
--- cnode-vue/src/pages/home/home.vue
+++ cnode-vue/src/pages/home/home.vue
@@ -1,46 +1,113 @@
 <template>
   <div class="hello">
     <nv-header></nv-header>
-    <ul>
-      <li v-for='item in data' :key="item.id" >
-        <router-link :to='`/topics/${item.id}`'>
-          {{item.title}}
-        </router-link>
-      </li>
-    </ul>
+    <section class="cnode_home">
+      <nav class="cnode_home--nav">
+        <ul class="cnode_home--nav-ul">
+          <li @click='switchMenu(item.label,item.params)' v-for="item in menu" :key="item.label" :class="{'cnode_home--nav-ul_active': currently === item.label, 'cnode_home--nav-ul_li': show}">{{item.value}}</li>
+        </ul>
+      </nav> 
+      <ul>
+        <li v-for='item in data' :key="item.id" >
+          <router-link :to='`/topics/${item.id}`'>
+            {{item.title}}
+          </router-link>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
 import { mapMutations } from 'vuex';
 import { homeApi } from '@/api/index';
 
 export default {
-  name: 'HelloWorld',
+  name: 'home',
   data() {
     return {
       data: '',
-      params: {
-        page: 1,
-        limit: 10,
-        tab: 'share'
-      }
+      currently: '01',
+      show: true,
+      menu: [
+        {
+          label: '01',
+          value: '全部',
+          params: {
+            page: 1,
+            limit: 20,
+            tab: 'all',
+            mdrender: true
+          }
+        },
+        {
+          label: '02',
+          value: '精华',
+          params: {
+            page: 1,
+            limit: 20,
+            tab: 'good',
+            mdrender: true
+          }
+        },
+        {
+          label: '03',
+          value: '分享',
+          params: {
+            page: 1,
+            limit: 20,
+            tab: 'share',
+            mdrender: true
+          }
+        },
+        {
+          label: '04',
+          value: '问答',
+          params: {
+            page: 1,
+            limit: 20,
+            tab: 'ask',
+            mdrender: true
+          }
+        },
+        {
+          label: '05',
+          value: '招聘',
+          params: {
+            page: 1,
+            limit: 20,
+            tab: 'job',
+            mdrender: true
+          }
+        }
+      ]
     };
   },
   created() {
-    homeApi(this.params)
+    homeApi(this.menu[0].params)
       .then(res => {
         if (res.data.success) {
           this.data = res.data.data;
         }
       })
       .catch(e => {
-        console.log(e);
+        console.error(e);
       });
   },
   methods: {
-    ...mapMutations(['CHANGE_PARAMS'])
+    ...mapMutations(['CHANGE_PARAMS']),
+    switchMenu(label, params) {
+      this.currently = label;
+      homeApi(params)
+        .then(res => {
+          if (res.data.success) {
+            this.data = res.data.data;
+          }
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    }
   }
 };
 </script>
