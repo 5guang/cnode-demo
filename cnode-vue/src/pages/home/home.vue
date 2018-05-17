a51cdb9d3fb1ec1875d1f249aa3bcb394694ded9
static <15701379752@163.com>
2018-05-17 23:33:19 +0800

    首页完成

--- cnode-vue/src/pages/home/home.vue
+++ cnode-vue/src/pages/home/home.vue
@@ -7,23 +7,30 @@
           <li @click='switchMenu(item.label,item.params)' v-for="item in menu" :key="item.label" :class="{'cnode_home--nav-ul_active': currently === item.label, 'cnode_home--nav-ul_li': show}">{{item.value}}</li>
         </ul>
       </nav> 
-      <ul class="cnode_home-wrap">
-        <li v-for='item in data' :key="item.id" class="cnode_home-wrap_list">
+      <section class="cnode_home-wrap">
+        <div v-for='item in data' :key="item.id" class="cnode_home-wrap_list">
           <router-link :to='`/topics/${item.id}`' class="cnode_home-wrap_list_link">
             <div class="cnode_home-wrap_list_link--content">
-              <img class="cnode_home-wrap_list_link--content-img" :src="item.author.avatar_url" alt="">
+              <!-- TODO 跳转到用户详情 -->
+              <a href="#">
+                <img class="cnode_home-wrap_list_link--content-img" :src="item.author.avatar_url" alt="">
+              </a>
               <div class="cnode_home-wrap_list_link--content-title">
-                <p>
-                  <span class="cnode_home-wrap_list_link--content-title-tab" :class="{'cnode_home-wrap_list_link--content-title-top': item.top}">{{item.tab | menu}}</span>
-                </p>
+                  <p class="cnode_home-wrap_list_link--content-title_theme">
+                    <span class="cnode_home-wrap_list_link--content-title-tab" :class="{'cnode_home-wrap_list_link--content-title-top': item.top || item.good}">{{item.tab | menu | topOrGood(item.top,item.good)}}</span>
+                    <span class="cnode_home-wrap_list_link--content-title_reply"><i>{{item.reply_count}}</i>/{{item.visit_count}}</span>
+                  </p>
                 <p class="cnode_home-wrap_list_link--content-title_txt">
-                  {{item.title}}
+                    {{item.title}}
                 </p>
+                <time class="cnode_home-wrap_list_link--content-title_time">
+                  {{item.last_reply_at | lastActiveTime(true)}}
+                </time>
               </div>
             </div>
           </router-link>
-        </li>
-      </ul>
+        </div>
+      </section>
     </section>
   </div>
 </template>
@@ -31,6 +38,8 @@
 <script>
 import { mapMutations } from 'vuex';
 import { homeApi } from '@/api/index';
+import { MillisecondToDate } from '@/utils/util';
+
 
 export default {
   name: 'home',
@@ -108,10 +117,10 @@ export default {
     menu(val) {
       let res = 'dsag'
       const menuList = [
-        {label: 'share', value: '分享'},
-        {label: 'good', value: '精华'},
-        {label: 'ask', value: '问答'},
-        {label: 'job', value: '招聘'},
+        {label: 'share', value: '分享', top: '置顶'},
+        {label: 'good', value: '精华', top: '置顶'},
+        {label: 'ask', value: '问答', top: '置顶'},
+        {label: 'job', value: '招聘', top: '置顶'},
       ]
       menuList.forEach(item => {
         if (item.label === val) {
@@ -120,6 +129,17 @@ export default {
       })
       return res;
     },
+    topOrGood(val, isTop, isGood) {
+      if (isTop) {
+        val = '置顶';
+      } else if (isGood) {
+        val = '精华';
+      }
+      return val;
+    },
+    lastActiveTime(time) {
+      return MillisecondToDate(time);
+    }
   },
   methods: {
     ...mapMutations(['CHANGE_PARAMS']),
